d4cccd5cf06b3d8ef7310b7f1e5ff15a34b95d76
Brian Durand <bbdurand@gmail.com>
2022-03-27 19:23:40 -0700

    documentation

--- lib/double_restraint.rb
+++ lib/double_restraint.rb
@@ -3,17 +3,39 @@
 require "restrainer"
 
 class DoubleRestraint
-  def initialize(name, timeout:, limit:, long_running_timeout:, long_running_limit:, timeout_errors: [TimeoutError], redis: nil)
+  # @param name [String, Symbol] The name of the restraint.
+  # @param timeout [Numeric] The first timeout that will be yielded to the block.
+  # @param long_running_timeout [Numeric] The timeout that will be yielded to the block if
+  #        the block times out the first time it is excuted.
+  # @param long_running_limit [Integer] The maximum number of times the block can be run
+  #        with the long running timeout across all processes.
+  # @param limit [Integer] The maximum of number of times the block can be run with the initial
+  #        timeout across all processes.
+  # @param timeout_errors [Array<Module>] List of errors that will be considered a timeout.
+  #        This needs to be customized depending on what the code in the block could throw to
+  #        indicate a timeout has occurred.
+  # @param redis [Redis] Redis connection to use.
+  #        If this is not set, the default value set for `Restrainer.redis` will be used.
+  def initialize(name, timeout:, long_running_timeout:, long_running_limit:, limit: nil, timeout_errors: [Timeout::Error], redis: nil)
     @timeout = timeout
     @long_running_timeout = long_running_timeout
     @timeout_errors = Array(timeout_errors)
-    @restrainer = Restrainer.new("DoubleRestrainer(#{name})", limit: limit, redis: redis)
+    @restrainer = Restrainer.new("DoubleRestrainer(#{name})", limit: limit, redis: redis) if limit
     @long_running_restrainer = Restrainer.new("DoubleRestrainer(#{name}).long_running", limit: long_running_limit, redis: redis)
   end
 
+  # Execute a block of code. The block will be yielded with the timeout value. If the block raises
+  # a timeout error, then it will be called again with the long running timeout. The code in the block
+  # must be idempotent since it can be run twice.
+  # @yieldparam [Numeric] the timeout value to use in the block.
+  # @raise [Restrainer::ThrottleError] if too many concurrent processes are trying to use the restraint.
   def execute
     begin
-      @restrainer.throttle do
+      if @restrainer
+        @restrainer.throttle do
+          yield @timeout
+        end
+      else
         yield @timeout
       end
     rescue => e
--- spec/double_restraint_spec.rb
+++ spec/double_restraint_spec.rb
@@ -12,6 +12,14 @@ describe DoubleRestraint do
     expect(retval).to eq 1
   end
 
+  it "should not require a limit" do
+    restraint = DoubleRestraint.new(:test, timeout: 1, long_running_timeout: 3, long_running_limit: 2)
+    retval = restraint.execute do |timeout|
+      timeout
+    end
+    expect(retval).to eq 1
+  end
+
   it "should restrain the number of processes executing at once" do
     threads = []
     begin
@@ -29,7 +37,7 @@ describe DoubleRestraint do
     timeouts = []
     restraint.execute do |timeout|
       timeouts << timeout
-      raise TimeoutError if timeouts.size == 1
+      raise Timeout::Error if timeouts.size == 1
     end
     expect(timeouts).to eq [1, 3]
   end
@@ -37,10 +45,10 @@ describe DoubleRestraint do
   it "should restrain the number of long running processes" do
     threads = []
     begin
-      threads << Thread.new { restraint.execute { |timeout| timeout == 1 ? raise(TimeoutError) : sleep(0.1) } }
-      threads << Thread.new { restraint.execute { |timeout| timeout == 1 ? raise(TimeoutError) : sleep(0.1) } }
+      threads << Thread.new { restraint.execute { |timeout| timeout == 1 ? raise(Timeout::Error) : sleep(0.1) } }
+      threads << Thread.new { restraint.execute { |timeout| timeout == 1 ? raise(Timeout::Error) : sleep(0.1) } }
       sleep(0.05)
-      expect { restraint.execute { |timeout| raise TimeoutError if timeout == 1 } }.to raise_error(Restrainer::ThrottledError)
+      expect { restraint.execute { |timeout| raise Timeout::Error if timeout == 1 } }.to raise_error(Restrainer::ThrottledError)
     ensure
       threads.each { |thread| thread.join }
     end
